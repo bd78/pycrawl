3e0e92f1286a28acb283c726e5a131a513111726
Bruno <bruno@MacBook-Pro-de-Bruno.local>
2016-04-01 08:53:30 +0200

    test01 in progress

---
+++ test01/test01/spiders/test01_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+from test01.items import Test01Item
+
+class Test01Spider(scrapy.Spider):
+    name = "test01"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = Test01Item()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
